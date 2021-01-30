c273a21172440193c9471bb040b356e5a47b820f
alvinadan111 <77431054+alvinadan111@users.noreply.github.com>
2021-01-30 17:29:22 +0800

    updateSQL

--- UI/menu.php
+++ UI/menu.php
@@ -37,7 +37,7 @@ endif;
         <div class="sidenav">
           <h1> MENU </h1>
           <a href="menu.php">Home</a>
-          <a href="facultyloading.html">Faculty Loading </a>
+          <a href="facultyloading.php">Faculty Loading </a>
           <button class="dropdown-btn"><i class="fa fa-caret-down"></i> Entry </button>
           <div class="dropdown-container">
             <a href="academic.html"> Academic Program </a>
