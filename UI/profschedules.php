3943026228011bd865a65335ae90cf4092ce9c7b
MJDAmbeguia <ambeguiamj@gmail.com>
2021-02-11 20:31:59 +0800

    css

--- UI/profschedules.php
+++ UI/profschedules.php
@@ -16,13 +16,14 @@ $pdo=Database::connect();
 <!DOCTYPE html>
 <html>
     <head>
-        <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+      
         <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <script src="https://kit.fontawesome.com/a076d05399.js"></script>
         <link rel="stylesheet" href="schedules.css">
     </head>
     <body>
