437d1ea3044aa984d9dda71a4722682659c09eb3
ianberenguer15 <77439544+ianberenguer15@users.noreply.github.com>
2021-02-12 10:47:25 +0800

    db Update add secret question & ans.

--- database/class_scheduler.sql
+++ database/class_scheduler.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 19, 2021 at 01:39 PM
+-- Generation Time: Feb 12, 2021 at 03:45 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `academicprog` (
   `progCode` varchar(10) NOT NULL,
   `progName` varchar(50) NOT NULL,
   PRIMARY KEY (`acadProgID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Truncate table before insert `academicprog`
@@ -48,7 +48,8 @@ TRUNCATE TABLE `academicprog`;
 
 INSERT INTO `academicprog` (`acadProgID`, `progCode`, `progName`) VALUES
 (1, 'BSIT', 'Bachelor of Science in Information Technology'),
-(2, 'BSED', 'Bachelor of Secondary Education');
+(2, 'BSED', 'Bachelor of Secondary Education'),
+(3, 'BSHRM', 'Bachelor of Science in Hotel and Restaurant Mngmnt');
 
 -- --------------------------------------------------------
 
@@ -66,15 +67,31 @@ CREATE TABLE IF NOT EXISTS `account` (
   `rankID` int(5) UNSIGNED NOT NULL,
   `specializationID` int(5) UNSIGNED NOT NULL,
   `pw` varchar(20) NOT NULL,
+  `accessLevel` varchar(7) NOT NULL,
+  `secretQuestion` varchar(100) NOT NULL,
+  `answer` varchar(50) NOT NULL,
   PRIMARY KEY (`accountID`),
-  UNIQUE KEY `idNum` (`idNum`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  UNIQUE KEY `idNum` (`idNum`),
+  UNIQUE KEY `idNum_2` (`idNum`),
+  UNIQUE KEY `idNum_3` (`idNum`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Truncate table before insert `account`
 --
 
 TRUNCATE TABLE `account`;
+--
+-- Dumping data for table `account`
+--
+
+INSERT INTO `account` (`accountID`, `FName`, `MName`, `LName`, `idNum`, `dept`, `rankID`, `specializationID`, `pw`, `accessLevel`, `secretQuestion`, `answer`) VALUES
+(1, 'Vin', 'admin', 'admin', 'admin', 1, 1, 1, 'admin', 'admin', '', ''),
+(2, 'Adan', 'Alvin', 'A.', 'admin2', 1, 2, 4, 'admin', 'admin', '', ''),
+(3, 'prof', 'dweewf', 'prof', 'prof', 1, 3, 2, 'prof', 'prof', '', ''),
+(4, 'student', 'student', 'qwert', 'student', 1, 1, 3, 'student', 'student', '', ''),
+(5, 'Mary', 'Not', 'Doe', 'mary', 1, 2, 3, 'mary', 'prof', '', '');
+
 -- --------------------------------------------------------
 
 --
@@ -83,10 +100,10 @@ TRUNCATE TABLE `account`;
 
 CREATE TABLE IF NOT EXISTS `classroom` (
   `classroomID` int(5) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `roomNum` int(3) UNSIGNED NOT NULL,
-  `buildingCode` varchar(15) NOT NULL,
+  `roomNum` int(3) UNSIGNED DEFAULT NULL,
+  `buildingCode` varchar(15) DEFAULT NULL,
   PRIMARY KEY (`classroomID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Truncate table before insert `classroom`
@@ -99,7 +116,9 @@ TRUNCATE TABLE `classroom`;
 
 INSERT INTO `classroom` (`classroomID`, `roomNum`, `buildingCode`) VALUES
 (1, 301, 'IT Building'),
-(2, 302, 'Educ Building');
+(2, 302, 'Educ Building'),
+(3, 303, 'HS Building'),
+(4, 304, 'Admin Building');
 
 -- --------------------------------------------------------
 
@@ -133,20 +152,29 @@ INSERT INTO `complab` (`compLabID`, `isCompLab`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `course` (
-  `courseID` int(5) UNSIGNED NOT NULL,
+  `courseID` int(5) UNSIGNED NOT NULL AUTO_INCREMENT,
   `courseCode` varchar(10) NOT NULL,
   `courseName` varchar(50) NOT NULL,
   `lec` float(3,2) UNSIGNED NOT NULL,
   `lab` float(3,2) UNSIGNED NOT NULL,
   `totalUnits` float(3,2) UNSIGNED NOT NULL,
   PRIMARY KEY (`courseID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Truncate table before insert `course`
 --
 
 TRUNCATE TABLE `course`;
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`courseID`, `courseCode`, `courseName`, `lec`, `lab`, `totalUnits`) VALUES
+(1, 'Prog 1', 'Programming 1', 2.00, 1.00, 3.00),
+(2, 'PE 1', 'Physical Health', 2.00, 1.00, 3.00),
+(3, 'WebDev', 'Web Development', 2.00, 1.00, 3.00);
+
 -- --------------------------------------------------------
 
 --
@@ -155,22 +183,76 @@ TRUNCATE TABLE `course`;
 
 CREATE TABLE IF NOT EXISTS `coursescheduling` (
   `crsSchedID` int(5) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `acadProgID` int(5) UNSIGNED NOT NULL,
-  `levelID` int(5) UNSIGNED NOT NULL,
-  `periodID` int(5) UNSIGNED NOT NULL,
-  `curID` int(5) UNSIGNED NOT NULL,
-  `dayID` int(5) UNSIGNED NOT NULL,
-  `timeStartID` int(5) UNSIGNED NOT NULL,
-  `timeEndID` int(5) UNSIGNED NOT NULL,
   `classroomID` int(5) UNSIGNED NOT NULL,
+  `dayID` int(5) NOT NULL,
+  `timeStartID` int(5) NOT NULL,
+  `timeEndID` int(5) NOT NULL,
+  `secID` int(5) NOT NULL,
+  `curID` int(5) NOT NULL,
   PRIMARY KEY (`crsSchedID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Truncate table before insert `coursescheduling`
 --
 
 TRUNCATE TABLE `coursescheduling`;
+--
+-- Dumping data for table `coursescheduling`
+--
+
+INSERT INTO `coursescheduling` (`crsSchedID`, `classroomID`, `dayID`, `timeStartID`, `timeEndID`, `secID`, `curID`) VALUES
+(6, 2, 1, 7, 10, 3, 2),
+(21, 4, 1, 7, 10, 1, 3),
+(23, 3, 5, 7, 10, 2, 3),
+(24, 3, 5, 10, 11, 3, 3),
+(25, 4, 5, 15, 17, 2, 3),
+(26, 2, 4, 12, 13, 2, 3),
+(27, 2, 2, 8, 9, 1, 3),
+(28, 1, 1, 7, 10, 2, 3),
+(29, 2, 5, 8, 10, 2, 3),
+(30, 3, 4, 14, 17, 2, 3),
+(31, 3, 6, 18, 20, 1, 3),
+(32, 2, 4, 13, 16, 2, 3),
+(33, 2, 3, 11, 13, 2, 3),
+(34, 1, 5, 7, 9, 2, 3),
+(35, 3, 4, 10, 12, 3, 3),
+(36, 2, 5, 10, 11, 1, 3),
+(37, 3, 6, 14, 14, 3, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courseschedulingtemp`
+--
+
+CREATE TABLE IF NOT EXISTS `courseschedulingtemp` (
+  `crsSchedID` int(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `classroomID` int(5) UNSIGNED NOT NULL,
+  `dayID` int(5) NOT NULL,
+  `timeStartID` int(5) NOT NULL,
+  `timeEndID` int(5) NOT NULL,
+  `secID` int(5) NOT NULL,
+  `curID` int(5) NOT NULL,
+  PRIMARY KEY (`crsSchedID`)
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Truncate table before insert `courseschedulingtemp`
+--
+
+TRUNCATE TABLE `courseschedulingtemp`;
+--
+-- Dumping data for table `courseschedulingtemp`
+--
+
+INSERT INTO `courseschedulingtemp` (`crsSchedID`, `classroomID`, `dayID`, `timeStartID`, `timeEndID`, `secID`, `curID`) VALUES
+(6, 2, 1, 7, 10, 3, 2),
+(25, 4, 5, 15, 17, 2, 3),
+(29, 2, 5, 8, 10, 2, 3),
+(34, 1, 5, 7, 9, 2, 3),
+(38, 2, 6, 18, 19, 2, 3);
+
 -- --------------------------------------------------------
 
 --
@@ -192,13 +274,23 @@ CREATE TABLE IF NOT EXISTS `curriculum` (
   `compLabID` int(5) UNSIGNED NOT NULL,
   `totalUnits` float(3,2) UNSIGNED NOT NULL,
   PRIMARY KEY (`curID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Truncate table before insert `curriculum`
 --
 
 TRUNCATE TABLE `curriculum`;
+--
+-- Dumping data for table `curriculum`
+--
+
+INSERT INTO `curriculum` (`curID`, `syID`, `periodID`, `levelID`, `acadProgID`, `courseID`, `crsName`, `deptID`, `lec`, `lab`, `units`, `compLabID`, `totalUnits`) VALUES
+(1, 1, 1, 1, 1, 1, 'Programming 1', 1, 3.00, 3.00, 3.00, 1, 3.00),
+(2, 1, 1, 1, 1, 2, 'PE 1', 2, 3.00, 3.00, 3.00, 2, 3.00),
+(3, 1, 1, 1, 1, 3, 'MobileComp', 1, 2.00, 1.00, 3.00, 2, 3.00),
+(4, 1, 2, 1, 1, 3, 'Web Development', 1, 2.00, 1.00, 3.00, 2, 3.00);
+
 -- --------------------------------------------------------
 
 --
@@ -261,19 +353,32 @@ INSERT INTO `department` (`deptID`, `deptCode`, `deptName`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `facultyloading` (
-  `facLoadID` int(5) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `deptID` int(5) UNSIGNED NOT NULL,
-  `levelID` int(5) UNSIGNED NOT NULL,
-  `accountID` int(5) UNSIGNED NOT NULL,
-  `crsSched` int(5) UNSIGNED NOT NULL,
+  `facLoadID` int(5) NOT NULL AUTO_INCREMENT,
+  `crsSchedID` int(5) UNSIGNED NOT NULL,
+  `classroomID` int(5) UNSIGNED NOT NULL,
+  `dayID` int(5) NOT NULL,
+  `timeStartID` int(5) NOT NULL,
+  `timeEndID` int(5) NOT NULL,
+  `secID` int(5) NOT NULL,
+  `curID` int(5) NOT NULL,
+  `accountID` int(5) DEFAULT NULL,
   PRIMARY KEY (`facLoadID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Truncate table before insert `facultyloading`
 --
 
 TRUNCATE TABLE `facultyloading`;
+--
+-- Dumping data for table `facultyloading`
+--
+
+INSERT INTO `facultyloading` (`facLoadID`, `crsSchedID`, `classroomID`, `dayID`, `timeStartID`, `timeEndID`, `secID`, `curID`, `accountID`) VALUES
+(1, 1, 2, 2, 7, 9, 3, 1, 5),
+(8, 24, 3, 5, 10, 11, 3, 3, 3),
+(27, 23, 3, 5, 7, 10, 2, 3, 3);
+
 -- --------------------------------------------------------
 
 --
@@ -304,25 +409,25 @@ INSERT INTO `lvl` (`levelID`, `levelDesc`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `prd`
+-- Table structure for table `period`
 --
 
-CREATE TABLE IF NOT EXISTS `prd` (
+CREATE TABLE IF NOT EXISTS `period` (
   `periodID` int(5) UNSIGNED NOT NULL AUTO_INCREMENT,
   `periodDesc` varchar(30) NOT NULL,
   PRIMARY KEY (`periodID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 --
--- Truncate table before insert `prd`
+-- Truncate table before insert `period`
 --
 
-TRUNCATE TABLE `prd`;
+TRUNCATE TABLE `period`;
 --
--- Dumping data for table `prd`
+-- Dumping data for table `period`
 --
 
-INSERT INTO `prd` (`periodID`, `periodDesc`) VALUES
+INSERT INTO `period` (`periodID`, `periodDesc`) VALUES
 (1, '1st Semester'),
 (2, '2nd Semester');
 
@@ -370,7 +475,7 @@ CREATE TABLE IF NOT EXISTS `schoolyear` (
   `syID` int(5) UNSIGNED NOT NULL AUTO_INCREMENT,
   `schoolYR` varchar(10) NOT NULL,
   PRIMARY KEY (`syID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Truncate table before insert `schoolyear`
@@ -408,7 +513,7 @@ TRUNCATE TABLE `section`;
 --
 
 INSERT INTO `section` (`secID`, `acadProgID`, `levelID`, `section`) VALUES
-(1, 1, 2, '2A'),
+(1, 1, 1, '1A'),
 (2, 1, 2, '2B'),
 (3, 1, 2, '2C');
 
@@ -450,7 +555,7 @@ CREATE TABLE IF NOT EXISTS `timeend` (
   `timeEndID` int(5) UNSIGNED NOT NULL AUTO_INCREMENT,
   `timeEnd` varchar(10) NOT NULL,
   PRIMARY KEY (`timeEndID`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Truncate table before insert `timeend`
@@ -462,21 +567,20 @@ TRUNCATE TABLE `timeend`;
 --
 
 INSERT INTO `timeend` (`timeEndID`, `timeEnd`) VALUES
-(1, '7 am'),
-(2, '8 am'),
-(3, '9 am'),
-(4, '10 am'),
-(5, '11 am'),
-(6, '12 nn'),
-(7, '1 pm'),
-(8, '2 pm'),
-(9, '3 pm'),
-(10, '4 pm'),
-(11, '5 pm'),
-(12, '6 pm'),
-(13, '7 pm'),
-(14, '8 pm'),
-(15, '9 pm');
+(8, '8 am'),
+(9, '9 am'),
+(10, '10 am'),
+(11, '11 am'),
+(12, '12 nn'),
+(13, '1 pm'),
+(14, '2 pm'),
+(15, '3 pm'),
+(16, '4 pm'),
+(17, '5 pm'),
+(18, '6 pm'),
+(19, '7 pm'),
+(20, '8 pm'),
+(21, '9 pm');
 
 -- --------------------------------------------------------
 
@@ -488,7 +592,7 @@ CREATE TABLE IF NOT EXISTS `timestart` (
   `timeStartID` int(5) UNSIGNED NOT NULL AUTO_INCREMENT,
   `timeStart` varchar(10) NOT NULL,
   PRIMARY KEY (`timeStartID`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Truncate table before insert `timestart`
@@ -500,20 +604,20 @@ TRUNCATE TABLE `timestart`;
 --
 
 INSERT INTO `timestart` (`timeStartID`, `timeStart`) VALUES
-(1, '7 am'),
-(2, '8 am'),
-(3, '9 am'),
-(4, '10 am'),
-(5, '11 am'),
-(6, '12 nn'),
-(7, '1 pm'),
-(8, '2 pm'),
-(9, '3 pm'),
-(10, '4 pm'),
-(11, '5 pm'),
-(12, '6 pm'),
-(13, '7 pm'),
-(14, '8 pm');
+(7, '7 am'),
+(8, '8 am'),
+(9, '9 am'),
+(10, '10 am'),
+(11, '11 am'),
+(12, '12 nn'),
+(13, '1 pm'),
+(14, '2 pm'),
+(15, '3 pm'),
+(16, '4 pm'),
+(17, '5 pm'),
+(18, '6 pm'),
+(19, '7 pm'),
+(20, '8 pm');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
