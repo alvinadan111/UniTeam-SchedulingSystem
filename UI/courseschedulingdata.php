46511b379b3817a95b89320f99021e6917808d4c
MJDAmbeguia <ambeguiamj@gmail.com>
2021-02-15 18:47:44 +0800

    css

--- UI/courseschedulingdata.php
+++ UI/courseschedulingdata.php
@@ -186,7 +186,7 @@ if(isset($_GET['saveSubmitBtn'])){
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> 
-    <link rel="stylesheet" href="CourseSchedulingdata.css">
+    <link rel="stylesheet" href="courseSchedulingdata.css">
 </head>
 
 <body>
